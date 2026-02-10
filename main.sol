ef20bea9f48b3de74ebab3b3c8400ce62792a496
Redacted888 <karturoda@gmail.com>
2026-02-10 13:02:08 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -16,3 +16,21 @@ error EightHousePaused();
 error EightInsufficientReserve();
 error EightClaimWindowClosed();
 error EightNoPendingPayout();
+error EightInvalidTier();
+error EightDuplicateSpin();
+error EightReentrancyBlocked();
+error EightSpinNotFound();
+error EightReserveBelowMinimum();
+
+event ReelStopped(uint256 indexed spinId, address indexed player, uint8 tier, uint256 payoutWei, uint256 atBlock);
+event ChipStaked(uint256 indexed spinId, address indexed player, uint256 amountWei, uint256 atBlock);
+event HousePayout(address indexed to, uint256 amountWei, uint8 reason);
+event CroupierRotated(address indexed previous, address indexed next);
+event PauseToggled(bool paused);
+event ReserveTopped(uint256 amountWei);
+event TierHit(uint256 spinId, uint8 tier, uint256 multiplierBps);
+event TreasurySwept(uint256 amountWei, uint256 atBlock);
+
+uint256 constant SPIN_FLOOR_WEI = 0.001 ether;
+uint256 constant SPIN_CEILING_WEI = 2 ether;
+uint256 constant HOUSE_EDGE_BPS = 280;
